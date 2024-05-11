5d8c035f3be9b2da3a4415a081ba807bb431abf9
PedroHCorreia <149112597+PedroHCorreia@users.noreply.github.com>
2024-05-11 00:28:46 -0300

    Add files via upload

---
+++ ExchangeCriptoMoedas_Projeto1/src/exchangecriptomoedas_projeto1/ExchangeCriptoMoedas_Projeto1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exchangecriptomoedas_projeto1;
+
+import view.JLogin;
+
+/**
+ *
+ * @author tioli
+ */
+public class ExchangeCriptoMoedas_Projeto1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new JLogin().setVisible(true);
+    }
+    
+}
