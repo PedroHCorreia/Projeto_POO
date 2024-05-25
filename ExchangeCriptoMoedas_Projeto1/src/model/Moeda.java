7596dce41631a7e26b81ecb6688a5a554fe4052c
PedroHCorreia <149112597+PedroHCorreia@users.noreply.github.com>
2024-05-25 05:21:23 -0300

    Classe Moeda

--- ExchangeCriptoMoedas_Projeto1/src/model/Moeda.java
+++ ExchangeCriptoMoedas_Projeto1/src/model/Moeda.java
@@ -8,6 +8,60 @@ package model;
  *
  * @author tioli
  */
-public class Moeda {
+public class Moeda implements Tarifacao {
+    private String nome;
+    private Double txCompra, txVenda;
+    private Double cota;
+    private Double saldo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getTxCompra() {
+        return txCompra;
+    }
+
+    public void setTxCompra(Double txCompra) {
+        this.txCompra = txCompra;
+    }
+
+    public Double getTxVenda() {
+        return txVenda;
+    }
+
+    public void setTxVenda(Double txVenda) {
+        this.txVenda = txVenda;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Double getCota() {
+        return cota;
+    }
+
+    public void setCota(Double cota) {
+        this.cota = cota;
+    }
+    
+    @Override
+    public Double tarifaCompra(Double valor) {
+        return (valor-(valor*getTxCompra()));
+    }
+
+    @Override
+    public Double tarifaVenda(Double valor) {
+        return (valor-(valor*getTxVenda()));
+    }
     
 }
