cc3d21a46775003d72a331631e85f7605e7edfbb
PedroHCorreia <149112597+PedroHCorreia@users.noreply.github.com>
2024-05-25 05:23:34 -0300

    Classe Pessoa

--- ExchangeCriptoMoedas_Projeto1/src/model/Pessoa.java
+++ ExchangeCriptoMoedas_Projeto1/src/model/Pessoa.java
@@ -9,5 +9,31 @@ package model;
  * @author tioli
  */
 public class Pessoa {
+    private String nome, cpf;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    
     
 }
