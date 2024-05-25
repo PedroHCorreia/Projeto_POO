db991d4832d80d3e2a9487592a553df00db24c1d
PedroHCorreia <149112597+PedroHCorreia@users.noreply.github.com>
2024-05-25 05:22:58 -0300

    Classe Investidor

--- ExchangeCriptoMoedas_Projeto1/src/model/Investidor.java
+++ ExchangeCriptoMoedas_Projeto1/src/model/Investidor.java
@@ -8,6 +8,32 @@ package model;
  *
  * @author tioli
  */
-public class Investidor {
+public class Investidor extends Pessoa{
+    private String senha;
+    private Carteira carteira;
+
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Carteira getCarteira() {
+        return carteira;
+    }
+
+    public void setCarteira(Carteira carteira) {
+        this.carteira = carteira;
+    }
+
+    public Investidor() {
+    }
+
+    public Investidor(String nome, String cpf, String senha) {
+        super(nome, cpf);
+        this.senha = senha; 
+        carteira = new Carteira();
+    }
     
 }
